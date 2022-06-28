a9af7ce988297b21e336745268e0f77341314217
Evhenii Danchenko <danchenkoevhenii@gmail.com>
2022-06-29 00:45:23 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+/*
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>
+*/
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery')
+
+
+const galleryMarkup = galleryItems.map(({preview, original, description})=> {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+}).join('')
+
+galleryRef.innerHTML = galleryMarkup;
+
+let imgOriginalLink = ''
+
+galleryRef.addEventListener('click', e => { 
+    e.preventDefault();
+    
+    imgOriginalLink = e.target.dataset.source;
+    }
+)
