17282bba0c37b12a627de0dd8dbff7b70b321161
Evhenii Danchenko <danchenkoevhenii@gmail.com>
2022-07-02 23:18:44 +0300

    add task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+/*
+<a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a>
+*/
+
+const galleryRef = document.querySelector('.gallery')
+
+
+const galleryMarkup = galleryItems.map(({preview, original, description})=> {
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}).join('')
+
+galleryRef.innerHTML = galleryMarkup;
+
+galleryRef.addEventListener('click', e => {
+    e.preventDefault();
+})
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
